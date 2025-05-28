dd543cc83b6473c5553adaf1a52b37d8782b7acb
starry <115192496+sky22333@users.noreply.github.com>
2025-05-28 21:43:36 +0800

    Update git.py

--- github/git.py
+++ github/git.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# 批量替换github仓库提交信息的用户名和邮箱，支持 HTTPS + Token 自动认证推送，需要安装apt install git-filter-repo
+# 批量替换github仓库提交信息的用户名和邮箱，支持 HTTPS + Token 自动认证推送，需要安装 "apt install git-filter-repo git -y"
 
 import os
 import shutil
